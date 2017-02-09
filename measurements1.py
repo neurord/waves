52d35776dca5c73a313d1811f6bc8b1a350c0aba
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2017-02-09 11:50:58 -0500

    Use more general names for the measurements

--- measurements1.py
+++ measurements1.py
@@ -3,17 +3,39 @@ from ajustador.loader import Measurement
 
 dirname = os.path.dirname(__file__)
 
-waves1 = Measurement(dirname + '/042811-6ivifcurves_Waves', bad_extra=('2',))
-waves2 = Measurement(dirname + '/042911-10ivifcurves_Waves', bad_extra=('2',))
-waves3 = Measurement(dirname + '/050311-4ivifcurves_Waves', IF=(300e-12, 20e-12))
-waves4 = Measurement(dirname + '/050411-7ivifcurves_Waves', IF=(300e-12, 20e-12), bad_extra=('2',))
-waves5 = Measurement(dirname + '/050511-3ivifcurves_Waves')
-waves6 = Measurement(dirname + '/050611-5ivifcurves_Waves', IF=(300e-12, 20e-12))
-waves7 = Measurement(dirname + '/051311-9ivifcurves_Waves', IF=(100e-12, 20e-12))
-waves8 = Measurement(dirname + '/051411-5ivifcurves_Waves')
-waves9 = Measurement(dirname + '/051811-13ivifcurves_Waves')
-waves10 = Measurement(dirname + '/090612-1ivcurves_Waves', IF=(-500e-12, 50e-12))
-waves11 = Measurement(dirname + '/091312-4ivcurves_Waves')
+waves042811 = Measurement(dirname + '/042811-6ivifcurves_Waves', bad_extra=('2',))
+waves1 = waves042811
+waves042911 = Measurement(dirname + '/042911-10ivifcurves_Waves', bad_extra=('2',))
+waves2 = waves042911
+waves050311 = Measurement(dirname + '/050311-4ivifcurves_Waves', IF=(300e-12, 20e-12))
+waves3 = waves050311
+waves050411 = Measurement(dirname + '/050411-7ivifcurves_Waves', IF=(300e-12, 20e-12), bad_extra=('2',))
+waves4 = waves050411
+waves050511 = Measurement(dirname + '/050511-3ivifcurves_Waves')
+waves5 = waves050511
+waves050611 = Measurement(dirname + '/050611-5ivifcurves_Waves', IF=(300e-12, 20e-12))
+waves6 = waves050611
+waves051311 = Measurement(dirname + '/051311-9ivifcurves_Waves', IF=(100e-12, 20e-12))
+waves7 = waves051311
+waves051411 = Measurement(dirname + '/051411-5ivifcurves_Waves')
+waves8 = waves051411
+waves051811 = Measurement(dirname + '/051811-13ivifcurves_Waves')
+waves9 = waves051811
+waves090612 = Measurement(dirname + '/090612-1ivcurves_Waves', IF=(-500e-12, 50e-12))
+waves10 = waves090612
+waves091312 = Measurement(dirname + '/091312-4ivcurves_Waves')
+waves11 = waves091312
 
-waves = (waves1, waves2, waves3, waves4, waves5, waves6, waves7,
-         waves8, waves9, waves10, waves11)
+waves = {
+    'waves042811':waves042811,
+    'waves042911':waves042911,
+    'waves050311':waves050311,
+    'waves050411':waves050411,
+    'waves050511':waves050511,
+    'waves050611':waves050611,
+    'waves051311':waves051311,
+    'waves051411':waves051411,
+    'waves051811':waves051811,
+    'waves090612':waves090612,
+    'waves091312':waves091312,
+}
