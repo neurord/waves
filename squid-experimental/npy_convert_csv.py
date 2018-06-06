4bf3f11c6a7b284cfabc6089bcc01cc08195d99a
sriram161 <skappag@masonlive.gmu.edu>
2018-06-06 17:38:06 -0400

    Generated squid_experimental data from npygit add .!

--- squid-experimental/npy_convert_csv.py
+++ squid-experimental/npy_convert_csv.py
@@ -1,7 +1,11 @@
 import numpy as np
 import pandas as pd
+from collections import OrderedDict
 
 data_file = "squid_vm1.npy"
 np_data_obj = np.load(data_file)
-data_frame = pd.DataFrame(np_data_obj)
-data_frame.to_csv("squid_experimental.csv")
+time_instances = np.linspace(0,0.9,4501)
+data = {'time(ms)': time_instances, 'i(1nA)':np_data_obj}
+data = OrderedDict(data.items(), key= lambda t:t[0])
+data_frame = pd.DataFrame(data).drop('key', axis=1)
+data_frame.to_csv("squid_experimental.csv", index=False)
