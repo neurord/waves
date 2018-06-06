44204e1be8885a9c22ce915378bf87392f6ff2b2
sriram161 <skappag@masonlive.gmu.edu>
2018-06-06 16:20:40 -0400

    sync with my_repo sriram161

---
+++ squid_experimental.py
@@ -0,0 +1,34 @@
+import pathlib
+import glob
+from ajustador.loader import CSVSeries
+
+class Params:
+    requires = ()
+    provides = ('baseline_before', 'baseline_after',
+                'steady_after', 'steady_before', 'steady_cutoff',
+                'falling_curve_window', 'rectification_window',
+                'injection_start', 'injection_end',
+                'injection_interval')
+
+    injection_start = 0.02 #Realworld experimental injection current in current clamp experiment.
+    injection_end = 0.06
+
+    injection_interval = injection_end - injection_start
+
+    baseline_before = None
+    baseline_after = injection_end + .1 # Going back to stability requires a bit of time
+                                        # Also skip the initial spike which is different.
+
+    steady_after = .500
+    steady_cutoff = 80
+    steady_before = injection_end
+
+    falling_curve_window = 20
+    rectification_window = 11
+
+dirname = pathlib.Path(__file__).parent / 'squid-experimental'
+csvs = sorted(glob.glob('{}/*.csv'.format(dirname)))
+
+params = Params()
+data = [CSVSeries(name, params) for name in csvs]
+data = {series.name:series for series in data}
