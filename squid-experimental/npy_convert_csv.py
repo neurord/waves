c2b5c9a2a9b2cf07ad50cfa3ac7b2f0b392f5d59
sriram161 <skappag@masonlive.gmu.edu>
2018-06-12 15:43:34 -0400

    Modified npy_convert_csv.py and made it generic.

--- squid-experimental/npy_convert_csv.py
+++ squid-experimental/npy_convert_csv.py
@@ -2,16 +2,16 @@ import numpy as np
 import pandas as pd
 from collections import OrderedDict
 
-def convert_sim_data_to_csv(data_file):
+def convert_sim_data_to_csv(data_file, output_file):
     ''' Created a csv file from the simulated data file generated from adjustador.basic_simulation.py when
         --save-vm argument is set in command line mode.
     '''
     np_data_obj = np.load(data_file).item()
-    time_instances = np.linspace(0,np_data_obj['simtime'],np_data_obj['data_points'])
-    data = {'time(ms)': time_instances, 'i({}A)'.format(np_data_obj['injection_current']):np_data_obj['voltage_trace']}
+    time_instances = np.linspace(0,np_data_obj['simtime'],np_data_obj['data_points_count'])
+    data = {'time(ms)': time_instances, 'i({}A)'.format(np_data_obj['injection_current']):np_data_obj['voltage_data_points']}
     data = OrderedDict(data.items(), key= lambda t:t[0])
     data_frame = pd.DataFrame(data).drop('key', axis=1)
-    data_frame.to_csv("squid_experimental.csv", index=False)
+    data_frame.to_csv(output_file, index=False)
 
-convert_sim_data_to_csv(data_file = "squid_vm1.npy")
+convert_sim_data_to_csv(data_file = "squid_vm1.npy", output_file = "squid_experimental.csv")
 
