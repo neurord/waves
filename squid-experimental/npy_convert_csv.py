2d69670367c1c7a3955f067b37fe754389d0f0b2
sriram161 <skappag@masonlive.gmu.edu>
2018-06-27 13:34:44 -0400

    Fixed time scale.L

--- squid-experimental/npy_convert_csv.py
+++ squid-experimental/npy_convert_csv.py
@@ -8,6 +8,7 @@ def convert_sim_data_to_csv(data_file, output_file, time_units='S', current_unit
     '''
     np_data_obj = np.load(data_file).item()
     time_instances = np.linspace(0,np_data_obj['simtime'],np_data_obj['data_points_count'])
+    time_instances = time_instances*1000 # should not do this way # FIXME
     data = {'Time '+time_units: time_instances,
             '{} {}'.format(np_data_obj['injection_current'][inj_indx], current_units): np_data_obj['voltage_data_points']}
     data = OrderedDict(data.items(), key= lambda t:t[0])
